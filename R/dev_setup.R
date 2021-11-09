438a0ea90d571239fab4bc7c4b08fb43b8a742d9
Evgeniya Gorobets <evgeniya.a.gorobets@gmail.com>
2021-11-09 09:48:56 -0500

    Added dev_setup for common package development functions

---
+++ R/dev_setup.R
@@ -0,0 +1,27 @@
+# System setup for development
+
+# Initial setup:
+# Run this every time you're about to start working on the package
+install.packages(c("devtools", "roxygen2", "testthat", "knitr", "usethis"))
+devtools::has_devel()
+
+# To update documenation:
+devtools::document()
+
+# To check the package:
+devtools::test()
+devtools::check()
+
+# TO load the package:
+devtools::load_all()
+
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#   Load Package:              'Ctrl + Shift + L'
