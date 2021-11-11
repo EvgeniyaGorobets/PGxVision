9edf7220f092aaa193a4ac5c8ba77b5aef1be01f
Evgeniya Gorobets <evgeniya.a.gorobets@gmail.com>
2021-11-11 15:14:51 -0500

    Added documentation to buildVolcanoPlot function

--- R/volcanoPlot.R
+++ R/volcanoPlot.R
@@ -1,21 +1,30 @@
-# For development only, load sample data
-if (sys.nframe() == 0) {
-  # development code here
-  source("R/utils.R")
-
-  # TODO: example data should be saved as an R obj? why?
-  biomarkerFile <- "data/pharmacodb_biomarkers_by_tissue.csv"
-  biomarkerDf <- read.csv(biomarkerFile)
-
-  experiment <- c("Lung", "Trametinib", "rna")
-  names(experiment) <- c("tissue", "compound", "mDataType")
-}
-
+#' Build a volcano plot of biomarkers
+#'
+#' Note that this volcano plot is NOT plotting results of differential gene
+#' expression analysis but is plotting the drug sensitivity signature
+#'
+#' @param biomarkerDf A data.frame of drug sensitivity biomarkers that you want
+#' to plot, including the compound, tissue, and molecular data type used in the
+#' experiment, the genomic coordinates of the genes which were tested on, and
+#' the resulting p-value/fdr and estimate
+#' @param experiment A named character vector representing the experiment for
+#' which you want to plot biomarkers; an experiment is defined by a "tissue",
+#' "compound", and "mDataType" (molecular data type)
+#' @return A ggplot2 plot object mapping the biomarkers of the experiment
+#' (x-axis = genome position; y-axis = p-value or fdr)
+#'
+#' @examples
+#' data(Biomarkers)
+#' experiment <- c("Lung", "Trametinib", "rna")
+#' names(experiment) <- c("tissue", "compound", "mDataType")
+#' buildVolcanoPlot(Biomarkers, experiment)
+#'
 #' @importFrom data.table setDT
-#' @importFrom ggplot2 ggplot, geom_point
+#' @importFrom ggplot2 ggplot geom_point scale_x_continuous guides theme aes scale_color_manual
+#' @importFrom ggprism guide_prism_minor
+#' @export
 buildVolcacoPlot <- function(biomarkerDf, experiment) {
-  # NOTE: not actually plotting results of differential analysis
-  # Used to look at drug sensitivity signature computed by PharmacoGx
+  #
 
   # TODO: check inputs
   # Convert biomarkerDf to data.table and extract relevant biomarkers
