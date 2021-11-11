0a8c841edffdca4ec422f75062f5ba2bddb612a9
Evgeniya Gorobets <evgeniya.a.gorobets@gmail.com>
2021-11-11 11:50:46 -0500

    Refactored the function to not rely on external chromosomes variable. Instead, the function can now be applied to any genome assembly, and chromosome names are lifted from the chromosomeDf parameter. The constraint placed on the data.frame param is that chromosomes must already be ordered according to the genome, and if an 'all' row is provided, then it must be the first row. May parameterize some of these settings at a later time but for now this is more stable.

--- R/manhattanPlot.R
+++ R/manhattanPlot.R
@@ -1,28 +1,42 @@
 # For development only, load sample data
 if (sys.nframe() == 0) {
   # development code here
-  # TODO: example data should be saved as an R obj? why?
-  biomarkerFile <- "data/pharmacodb_biomarkers_by_tissue.csv"
-  biomarkerDf <- read.csv(biomarkerFile)
+  source("R/utils.R")
 
-  # TODO: determine what part of this should be within the fxn
-  chromosomeData <- jsonlite::fromJSON("data/chromosome_lengths.json",
-                                       simplifyDataFrame = TRUE)
-  chromosomeDf <- chromosomeData$`Chromosome Info`
-  colnames(chromosomeDf)[colnames(chromosomeDf) == "value"] <- "chrLength"
-  colnames(chromosomeDf)[colnames(chromosomeDf) == "molecule-name"] <- "chrName"
-
-  experiment <- c("Lung", "Trametinib", "rna")
-  names(experiment) <- c("tissue", "compound", "mDataType")
+  # TODO: move to zzz.R
+  chromosomes <- c(1:22, "X", "Y")
 }
 
-# TODO: move to zzz.R
-chromosomes <- c(1:22, "X", "Y")
 
-#' @importFrom data.table setDT
-#' @importFrom ggplot2 ggplot, geom_point, scale_x_continuous, guides, theme
+#' Build a Manhattan plot of biomarkers
+#'
+#' @param biomarkerDf A data.frame of drug sensitivity biomarkers that you want
+#' to plot, including the compound, tissue, and molecular data type used in the
+#' experiment, the genomic coordinates of the genes which were tested on, and
+#' the resulting p-value/fdr
+#' @param chromosomeDf A data.frame of the reference genome, including the
+#' lengths of all the chromosomes. The row order should match the chromosome
+#' order in the reference genome. If there is an "all" row, it should be first.
+#' @param experiment A named character vector representing the experiment for
+#' which you want to plot biomarkers; an experiment is defined by a "tissue",
+#' "compound", and "mDataType" (molecular data type)
+#' @param relativeGenomeCoords A boolean that indicates whether the genomic
+#' coordinates given in biomarkerDf are relative to the chromosome (TRUE) or
+#' relative to the entire genome (absolute, FALSE). Default is TRUE.
+#' @return A ggplot2 plot object mapping the biomarkers of the experiment
+#' (x-axis = genome position; y-axis = p-value or fdr)
+#'
+#' @examples
+#' data(Biomarkers)
+#' data(GRCh38.p13.Assembly)
+#' experiment <- c("Lung", "Trametinib", "rna")
+#' names(experiment) <- c("tissue", "compound", "mDataType")
+#' buildManhattanPlot(Biomarkers, GRCh38.p13.Assembly, experiment, TRUE)
+#'
+#' @importFrom data.table setDT, copy
+#' @importFrom ggplot2 ggplot geom_point scale_x_continuous guides theme
 #' @importFrom ggprism guide_prism_minor
-# TODO: convert internally to data.table
+#' @export
 buildManhattanPlot <- function(biomarkerDf=NULL,
                                chromosomeDf=NULL,
                                experiment=NULL,
@@ -47,9 +61,16 @@ buildManhattanPlot <- function(biomarkerDf=NULL,
   setDT(biomarkerDf, keep.rownames=TRUE)
   setDT(chromosomeDf, keep.rownames=TRUE)
 
-  # Pre-process data
+  # Select drug sensitivity results from the experiment
   selectedBiomrks <- selectExperiment(biomarkerDf, experiment)
-  # If the provided genomic coordinates are relative, absolute-ize them
+
+  # If there is an "all" row in the chromosomeDf, remove it
+  if (chromosomeDf[1, chrName] == "all") {
+    chromosomeDf <- chromosomeDf[2:nrow(chromosomeDf)]
+  }
+
+  # If the provided genomic coordinates are relative to the chromosome,
+  # shift them to be relative to the genome and compute chromosome coords
   if (relativeGenomeCoords) {
     result <- absolutizeGenomicCoords(selectedBiomrks, chromosomeDf)
     selectedBiomrks <- result[[1]]
@@ -57,17 +78,20 @@ buildManhattanPlot <- function(biomarkerDf=NULL,
   }
 
   # Build the Manhattan plot
-  plot <- ggplot(selectedBiomrks, aes(x=abs_gene_seq_start, y=-log10(pvalue)))
+  totalGenomeLen <- chromosomeDf[nrow(chromosomeDf),
+                                 abs_gene_seq_start + chrLength]
+  plot <- ggplot(selectedBiomrks, aes(x=abs_gene_seq_start, y=-log10(pvalue),
+                                      xmin=1, xmax=totalGenomeLen))
   plot <- plot + geom_point()
 
   # Customize x-axis ticks and labels
   # TODO: add theme!
   midChromosome <- (chromosomeDf$seq_start + chromosomeDf$seq_end)/2
-  totalGenomeLen <- chromosomeDf[chrName == "all", chrLength]
+  chromosomeNames <- chromosomeDf$chrName
   plot <- plot + # theme(axis.text.x=element_text(family, face, colour, size)) +
     scale_x_continuous("Chromosome", breaks=midChromosome[-1],
         minor_breaks=c(chromosomeDf$seq_start, chromosomeDf$seq_end),
-        labels=chromosomes, limits=c(1, totalGenomeLen)) +
+        labels=chromosomes) +
     guides(x = guide_prism_minor())
 
   return(plot)
@@ -75,30 +99,29 @@ buildManhattanPlot <- function(biomarkerDf=NULL,
 
 
 absolutizeGenomicCoords <- function(selectedBiomrks, chromosomeDf) {
-  # TODO: update this to use a reference genome?
+  # Make a copy of the data.tables so you're not modifying user data
+  selectedBiomrks <- copy(selectedBiomrks)
+  chromosomeDf <- copy(chromosomeDf)
 
   # Add columns to track absolute position in genome
   chromosomeDf$seq_start <- 1
   chromosomeDf$seq_end <- chromosomeDf[chrName == "all", chrLength]
   selectedBiomrks$abs_gene_seq_start <- selectedBiomrks$gene_seq_start
 
-  # For each chromosome (except chr1), compute its absolute position in the
-  # genome and shift biomarker positions accordingly
-  for (chr in (seq_along(chromosomes[-1])+1)) {
-    # Get the absolute seq_start and seq_end of the chromosome
-    prevChrom <- chromosomeDf$chrName == chromosomes[chr-1]
-    prevChromEnd <- chromosomeDf$seq_start[prevChrom] + chromosomeDf$chrLength[prevChrom]
-    chromosomeDf$seq_end[prevChrom] <- prevChromEnd - 1
-
-    currentChrom <- chromosomeDf$chrName == chromosomes[chr]
-    chromosomeDf$seq_start[currentChrom] <- prevChromEnd
+  # Shift genomic coords to be relative to genome instead of each chromosome
+  chromEnd <- 0  # the end of the previous chromosome
+  for (row in 1:nrow(chromosomeDf)) {
+    # Set the absolute seq_start & seq_end of each chromosome
+    chromosomeDf[row, seq_start] <- chromEnd + 1
+    chromosomeDf[row, seq_end] <- chromEnd + chromosomeDf[row, chrLength]
 
     # Update all biomarkers in that chromosome with absolute gene_seq_start
-    # TODO: I am modifying in place? should make a copy instead?
-    chromName <- paste("chr", chromosomes[chr], sep="")
-    relativeSeqStart <- selectedBiomrks[chr == chromName, gene_seq_start]
-    absoluteSeqStart <- relativeSeqStart + prevChromEnd - 1
-    selectedBiomrks[chr == chromName, abs_gene_seq_start] <- absoluteSeqStart
+    chromName <- paste("chr", chromosomeDf[row, chrName], sep="") #TODO: not generic enough
+    selectedBiomrks[chr == chromName, abs_gene_seq_start] <-
+      selectedBiomrks[chr == chromName, gene_seq_start] + chromEnd
+
+    # Update chromEnd to end of current chromosome
+    chromEnd <- chromosomeDf[row, seq_end]
   }
 
   # Return the modified dfs
