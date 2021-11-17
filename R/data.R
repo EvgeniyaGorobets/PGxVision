16638ecbf2a184903c8eef9e1c801e504d5b2a9a
Evgeniya Gorobets <evgeniya.a.gorobets@gmail.com>
2021-11-16 20:19:25 -0500

    Updated README with image and updated data docs

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' @format A data frame with 38 rows and 3 variables:
 #' \describe{
 #'   \item{tumour}{The name of the PDX tumour}
-#'   \item{ODC1}{???}
+#'   \item{ODC1}{The transcript levels of the ODC1 gene in the tumour}
 #'   \item{angle}{The angle between the treatment and control}
 #' }
 #' @source Xeva. R package version 1.10.0.
@@ -16,15 +16,15 @@
 #' Drug sensitivity data for genes in the GRCh38.p13 genome assembly
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' diamonds.???
 #'
 #' @format A data frame with 7805 rows and 12 variables:
 #' \describe{
 #'   \item{gene}{ENSEMBL gene name}
 #'   \item{compound}{name of compound used in experiment}
 #'   \item{tissue}{name of tissue used in experiment}
-#'   \item{estimate}{???}
-#'   \item{n}{???}
+#'   \item{estimate}{The drug sensitivity estimate}
+#'   \item{n}{The number of trials}
 #'   \item{fdr}{false discovery rate (adjusted p-value)}
 #'   \item{mDataType}{the molecular data type, i.e., rna, etc.}
 #'   \item{gene_seq_start}{the relative starting position of the gene within
@@ -39,18 +39,15 @@
 "Biomarkers"
 
 
-#' Information about chromosomes in the GRCh38.p13 genome assembly
+#' GRCh38.p13 genome assembly
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing the characteristics of of the GRCh38.p13 genome
+#' assembly, specifically chromosome names and lengths.
 #'
-#' @format A data frame with 25 rows and 6 variables:
+#' @format A data frame with 25 rows and 3 variables:
 #' \describe{
-#'   \item{# unit-name}{???}
 #'   \item{chrName}{the name of the chromosome (all, 1-22, X, Y)}
 #'   \item{molecule-type/loc}{the molecule type ("chromosome" or "all")}
-#'   \item{sequence-type}{???}
-#'   \item{statistic}{the chromosome statistic measured in this row}
 #'   \item{chrLength}{the length of the chromosome (# nucleotides)}
 #' }
 #' @source \url{https://www.gencodegenes.org/pages/gencode.html}
